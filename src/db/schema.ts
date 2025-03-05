b20b98d38f7509364deaff7ae29364564b84bd00
100stacks <100stacks@users.noreply.github.com>
2025-03-05 14:58:39 -0500

    db: drizzle config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { integer, text, boolean, timestamp,pgTable } from "drizzle-orm/pg-core";
 
 export const todo = pgTable("todo", {
     id: integer("id").primaryKey(),
-    text: text("text").notNull(),
+    task: text("task").notNull(),
     done: boolean("done").default(false).notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at")
